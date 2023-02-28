a185486f6fbf286550e8454953680dcc32548eb3
matheustkaczyk <matheus111904@hotmail.com>
2023-02-28 10:14:14 -0300

    feat: add more distinct font size and weight on OBS

--- Gelo_Sao_Mateus_EMISSOR_NF/Form1.Designer.cs
+++ Gelo_Sao_Mateus_EMISSOR_NF/Form1.Designer.cs
@@ -378,6 +378,7 @@
             // 
             // obsTextBox
             // 
+            this.obsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.obsTextBox.Location = new System.Drawing.Point(11, 372);
             this.obsTextBox.Name = "obsTextBox";
             this.obsTextBox.Size = new System.Drawing.Size(604, 191);
